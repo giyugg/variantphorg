5118f9732e6790b3327212925b7c032912873586
Giyuuu <134776227+kyleacuna@users.noreply.github.com>
2024-04-04 19:28:50 +0800

    ermmmmmmmmmmmm

---
+++ osu!/tournaments/OST S2/tasklist.py
@@ -0,0 +1,15 @@
+def print_divider():
+    print("---------------------------------------------")
+
+def print_task(task_name, status):
+    print(f"{task_name}\nStatus: {status}")
+    print_divider()
+
+print_divider()
+print("TASK LIST FOR OST S2")
+print_divider()
+
+print_task("Markdown File for osu!Supporternament S2", "In progress.")
+
+print_task("BBCode File for osu!Supporternament S2",
+           "To be created again.\nReason: Current version sucks.")
